1dbc8b588ead05b44b0081cc1e368f0a6b19e39c
anthony-major <148173066+anthony-major@users.noreply.github.com>
2024-11-21 22:41:54 -0500

    Begin interpreting
    - Added interpreting of basic expressions
    - Error handling still needs to be done
    - Wiring up still needs to be done

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,9 +1,39 @@
-use std::io::Write;
+use std::{error::Error, fmt::Display, io::Write};
 
 use crate::{
+    ast::{Accept, Visitor},
     scanner::{Scanner, ScannerError},
     token::TokenKind,
 };
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum LoxValue {
+    Nil,
+    Boolean(bool),
+    Number(f64),
+    String(String),
+}
+
+impl LoxValue {
+    pub fn is_truthy(&self) -> bool {
+        match self {
+            LoxValue::Nil => false,
+            LoxValue::Boolean(b) => b.clone(),
+            _ => true,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct RuntimeError {}
+
+impl Error for RuntimeError {}
+
+impl Display for RuntimeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Ok(())
+    }
+}
 
 pub struct Interpreter {}
 
@@ -56,6 +86,82 @@ impl Interpreter {
 
         for err in errors {
             println!("{}", err);
+        }
+    }
+}
+
+impl Visitor for Interpreter {
+    type Result = Result<LoxValue, RuntimeError>;
+
+    fn visit_literal(&self, literal: &crate::ast::Literal) -> Self::Result {
+        match literal.value.kind() {
+            TokenKind::Nil => Ok(LoxValue::Nil),
+            TokenKind::True => Ok(LoxValue::Boolean(true)),
+            TokenKind::False => Ok(LoxValue::Boolean(false)),
+            TokenKind::Number(x) => Ok(LoxValue::Number(x.clone())),
+            TokenKind::String(s) => Ok(LoxValue::String(s.clone())),
+            _ => Err(RuntimeError {}),
+        }
+    }
+
+    fn visit_grouping(&self, grouping: &crate::ast::Grouping) -> Self::Result {
+        grouping.expression.accept(self)
+    }
+
+    fn visit_unary(&self, unary: &crate::ast::Unary) -> Self::Result {
+        let right = unary.right.accept(self)?;
+
+        match unary.operator.kind() {
+            TokenKind::Minus => match right {
+                LoxValue::Number(x) => Ok(LoxValue::Number(-x)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Bang => Ok(LoxValue::Boolean(!right.is_truthy())),
+            _ => Err(RuntimeError {}),
+        }
+    }
+
+    fn visit_binary(&self, binary: &crate::ast::Binary) -> Self::Result {
+        let left = binary.left.accept(self)?;
+        let right = binary.right.accept(self)?;
+
+        match binary.operator.kind() {
+            TokenKind::Minus => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Number(x - y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Slash => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Number(x / y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Star => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Number(x * y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Plus => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Number(x / y)),
+                (LoxValue::String(x), LoxValue::String(y)) => Ok(LoxValue::String(x + &y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Greater => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Boolean(x > y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::GreaterEqual => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Boolean(x >= y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::Less => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Boolean(x < y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::LessEqual => match (left, right) {
+                (LoxValue::Number(x), LoxValue::Number(y)) => Ok(LoxValue::Boolean(x <= y)),
+                _ => Err(RuntimeError {}),
+            },
+            TokenKind::BangEqual => Ok(LoxValue::Boolean(left != right)),
+            TokenKind::EqualEqual => Ok(LoxValue::Boolean(left == right)),
+            _ => Err(RuntimeError {}),
         }
     }
 }
